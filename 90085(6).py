609e36f44d874450631b5b4ede0277dbda69a2d5
Arthur1987921 <70871859+Arthur1987921@users.noreply.github.com>
2020-10-12 11:55:45 +0800

    Add files via upload

---
+++ 90085(6).py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 12 09:30:39 2020
+
+@author: cis-user
+"""
+
+# l1=[1,2,3,4,5]
+# l2=[6,5,4,3,2]
+# l3=[]
+# for i in range(0,4):
+#     if l1[i]>l2[i]:l3.append(l1[i])
+#     eles:l3.append(l2[i])
+# print(l3)
+
+# l1=[1,2,3,4,5]
+# l2=[6,5,4,3,2]
+# l3=[False,False,True,False,True]
+# l4=[]
+# for i in range(0,4):
+#     if l3[i]==True:l4.append(l1[i])
+#     eles:l4.append(l2[i])
+# print(l4)
